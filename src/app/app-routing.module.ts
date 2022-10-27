faf037f0cf060ac63af1712090dcfb867d78dafd
LILianHUb <lilian.ballejos@hotmail.fr>
2022-10-27 14:33:56 +0200

    installing angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  PageComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
