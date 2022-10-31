babd183eb8c141c523a7dd726494d12ce0da842d
LILianHUb <lilian.ballejos@hotmail.fr>
2022-10-31 13:12:02 +0100

    avancement accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,34 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {ElementAccueilComponent} from '../element-accueil/element-accueil.component';
 import { DataAccueilService } from '../services/data_accueil.service';
 import { ContenuAccueil } from '../model/contenu-accueil.model';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.scss']
+  styleUrls: ['./accueil.component.scss'],
 })
-export class AccueilComponent  implements OnInit {
+export class AccueilComponent implements OnInit {
   couleurBackground: string;
   presentations: ContenuAccueil[];
-  
+  loading: boolean = false;
 
   constructor(private dataaccueilservice: DataAccueilService) {}
-  
-	ngOnInit() {
-		this.couleurBackground = 'background-blue';
 
-    this.dataaccueilservice.getAllDataAccueil().subscribe(
-      (contenu: ContenuAccueil[]) => {
+  ngOnInit() {
+    this.couleurBackground = 'background-blue';
+
+    this.dataaccueilservice
+      .getAllDataAccueil()
+      .subscribe((contenu: ContenuAccueil[]) => {
         this.presentations = contenu;
-        //this.loading = true;
-      }
-    );
+        this.loading = true;
+      });
+
     
-
   }
-
 }
-
- 
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,7 @@ import {InputSwitchModule} from 'primeng/inputswitch';
     AccueilComponent,
     HeaderComponent,
     FooterComponent,
-    ElementAccueilComponent
+    ElementAccueilComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +32,7 @@ import {InputSwitchModule} from 'primeng/inputswitch';
     CarouselModule,
     SelectButtonModule,
     InputSwitchModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
